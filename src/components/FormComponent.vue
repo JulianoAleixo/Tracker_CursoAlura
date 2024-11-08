d4c39ae86f55308d7554bf10de7e2bf3f5ee5137
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2024-11-08 16:46:45 -0300

    :sparkles: Feat - Creating App, Form and SideBar

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,72 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "FormComponent",
+    data() {
+        return {
+            timeInSeconds: 0,
+            interval: 0
+        };
+    },
+    computed: {
+        timePassed(): string {
+            return new Date(this.timeInSeconds * 1000)
+                .toISOString()
+                .substr(11, 8);
+        },
+    },
+    methods: {
+        handleStart() {
+            this.interval = setInterval(() => {
+                this.timeInSeconds += 1;
+            }, 1000);
+        },
+        handleFinish() {
+            clearInterval(this.interval);
+        },
+    },
+});
+</script>
+
+<template>
+    <div class="box">
+        <div class="columns">
+            <div
+                class="column is-8"
+                role="form"
+                aria-label="Formulário para criação de uma nova tarefa."
+            >
+                <input
+                    type="text"
+                    class="input"
+                    placeholder="Qual tarefa você deseja iniciar?"
+                />
+            </div>
+            <div class="column">
+                <div
+                    class="is-flex is-align-items-center is-justify-content-space-between"
+                >
+                    <section>
+                        <strong>{{ timePassed }}</strong>
+                    </section>
+
+                    <button class="button" @click="handleStart()">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>Play</span>
+                    </button>
+                    <butotn class="button" @click="handleFinish()">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>Stop</span>
+                    </butotn>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "SideBar",
+});
+</script>
+
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.jpg" alt="" class="sidebar-logo" />
+        </h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+    background: #0d3b66;
+    width: 100%;
+    padding: 1rem;
+    height: 100vh;
+}
+
+.sidebar-logo {
+    margin: auto;
+    height: auto;
+    border-radius: 10px;
+    object-fit: contain;
+    display: block;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
